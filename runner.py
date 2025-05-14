de6b4e1ed33a2c1b06a0bd1ff814ae628ebac59f
ottodanp <me@ottodanp.dev>
2025-05-14 03:04:44 +0100

    added inserted/skipped counter to runner display

--- runner.py
+++ runner.py
@@ -7,25 +7,26 @@ from aiohttp import ClientSession
 from data import VideoData
 from youtube import load_trending_videos
 
-RE_SCRAPE_DELAY = 60 * 30
+RE_SCRAPE_DELAY = 60 * 10
 
 
 def sanitize(item: str) -> str:
     return item.replace('"', '')
 
 
-def insert_video_data(connection: Connection, video: VideoData, category: str):
+def insert_video_data(connection: Connection, video: VideoData, category: str)-> bool :
     timestamp = datetime.now().timestamp()
     cur = connection.cursor()
     query = f"""SELECT * FROM videos WHERE video_id = \"{video.video_id}\""""
     cur.execute(query)
     if len(cur.fetchall()) != 0:
-        return
+        return False
     print("inserting " + video.video_id)
     query = f"""INSERT INTO videos (video_id, title, channel_name, view_count, duration, upload_time, thumbnail_url, description, keywords, category, timestamp)
     VALUES ("{video.video_id}", "{sanitize(video.title)}", "{video.channel_name}", "{video.view_count}", "{video.duration}", "{video.upload_time}", "{video.thumbnail_url}", "{sanitize(video.description)}", "{video.keyword_string}", "{category}", {timestamp})"""
     cur.execute(query)
     connection.commit()
+    return True
 
 
 def create_tables(connection: Connection):
@@ -50,13 +51,18 @@ def create_tables(connection: Connection):
 async def main():
     db = connect("trending.db")
     create_tables(db)
+    inserted = 0
+    skipped = 0
     async with ClientSession() as session:
         videos = await load_trending_videos(session)
         for cat in videos:
             for video in videos[cat]:
-                insert_video_data(db, video, cat)
+                if not insert_video_data(db, video, cat):
+                    skipped += 1
+                else:
+                    inserted += 1
 
-        print("done")
+        print(f"done. inserted: {inserted}, skipped: {skipped}")
         await sleep(RE_SCRAPE_DELAY)
 
 
